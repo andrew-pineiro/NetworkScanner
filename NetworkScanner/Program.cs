d4767fb9b46be29d6bb5ffa2281126c7c49c9125
andrew-pineiro <apineiro@towerpower.com>
2023-08-06 13:44:19 -0400

    Add project files.

---
+++ NetworkScanner/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.NetworkInformation;
+using System.Text;
+
+namespace NetworkScanner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = SendPing(args[0]);
+            if(result == IPStatus.Success)
+            {
+                Console.WriteLine("success!");
+            } else
+            {
+                Console.WriteLine(result);
+            }
+        }
+        static IPStatus SendPing(string address)
+        {
+            Ping sender = new();
+            PingOptions options = new();
+
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120;
+            PingReply reply = sender.Send(address, timeout, buffer, options);
+            return reply.Status;
+        }
+    }
+}
